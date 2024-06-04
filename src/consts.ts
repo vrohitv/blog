5b60d162c0eae083364ae4ef8c84c292d696e13d
Vemuri Venkat Rohit <contact.venkatrohit@gmail.com>
2024-06-03 22:37:21 -0700

    First Draft

--- src/consts.ts
+++ src/consts.ts
@@ -18,12 +18,12 @@ type SocialLink = {
 }
 
 export const siteConfig: Config = {
-  title: "Astro Theme Cody",
+  title: "Venkat Rohit Vemuri",
   description: "",
   lang: "en-GB",
   profile: {
-    author: "Amy Dang",
-    description: "your bio description"
+    author: "Venkat Rohit Vemuri",
+    description: "Software Engineer"
   }
 }
 
@@ -36,22 +36,17 @@ export const socialLinks: Array<SocialLink> = [
   {
     icon: "mdi:github",
     friendlyName: "Github",
-    link: "https://github.com/kirontoo/astro-theme-cody",
+    link: "https://github.com/vvrm",
   },
   {
     icon: "mdi:linkedin",
     friendlyName: "LinkedIn",
-    link: "#",
+    link: "https://www.linkedin.com/in/v-v-r/",
   },
   {
     icon: "mdi:email",
     friendlyName: "email",
-    link: "mailto:ndangamy@gmail.com",
-  },
-  {
-    icon: "mdi:rss",
-    friendlyName: "rss",
-    link: "/rss.xml"
+    link: "mailto:contact.venkatrohit@gmail.com",
   }
 ];
 
@@ -68,12 +63,4 @@ export const NAV_LINKS: Array<{ title: string, path: string }> = [
     title: "Blog",
     path: "/blog",
   },
-  {
-    title: "Projects",
-    path: '/projects'
-  },
-  {
-    title: "Archive",
-    path: '/archive'
-  }
 ];
